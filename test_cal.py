5dcca00fd001f89b209e006a48d839416382c52e
mm123 <mm0101@gmail.com>
2026-06-30 13:34:58 +0530

    Initial project

---
+++ test_cal.py
@@ -0,0 +1,13 @@
+from cal import *
+
+def test_add():
+    assert add(2,3)==5
+
+def test_subtract():
+    assert subtract(10,4)==6
+
+def test_multiply():
+    assert multiply(5,4)==20
+
+def test_divide():
+    assert divide(20,5)==4
